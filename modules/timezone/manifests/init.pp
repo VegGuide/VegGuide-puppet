5ac78639d313589ca7ccaabe80d5c9f540868e2e
John Thompson <jthompson@maxmind.com>
2014-09-07 12:21:27 -0500

    Hopefully make timezone stick as mountain time

--- modules/timezone/manifests/init.pp
+++ modules/timezone/manifests/init.pp
@@ -3,4 +3,9 @@ class timezone {
         ensure  => file,
         content => "America/Denver\n",
     }
+
+    file { '/etc/localtime':
+        ensure => link,
+        target => '/usr/share/zoneinfo/America/Denver',
+    }
 }
